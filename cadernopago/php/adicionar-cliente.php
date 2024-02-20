c9df8932381ecd37392c7c0025bfe3e11dfaf42b
fguilhermeICLS <146739245+fguilhermeICLS@users.noreply.github.com>
2024-02-20 11:44:15 -0300

    Update adicionar-cliente.php

--- cadernopago/php/adicionar-cliente.php
+++ cadernopago/php/adicionar-cliente.php
@@ -4,27 +4,65 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Adicionar Clientes</title>
+    <script>
+        function formatarCPF(cpf) {
+            let cpfFormatado = cpf.replace(/\D/g, '');
+            cpfFormatado = cpfFormatado.replace(/(\d{3})(\d)/, '$1.$2'); 
+            cpfFormatado = cpfFormatado.replace(/(\d{3})(\d)/, '$1.$2'); 
+            cpfFormatado = cpfFormatado.replace(/(\d{3})(\d{1,2})$/, '$1-$2');
+
+            return cpfFormatado;
+        }
+
+        function atualizarCampoCPF() {
+            var campoCPF = document.getElementById('cpf');
+            campoCPF.value = formatarCPF(campoCPF.value);
+        }
+
+        function formatarDivida(divida) {
+            let dividaFormatada = divida.replace(/\D/g, '');
+            dividaFormatada = dividaFormatada.replace(/(\d{1,})(\d{2})$/, '$1.$2');
+
+            return dividaFormatada;
+        }
+
+        function atualizarCampoDivida() {
+            var campoDivida = document.getElementById('divida');
+            campoDivida.value = formatarDivida(campoDivida.value);
+        }
+
+        function formatarTelefone(telefone) {
+            let telefoneFormatado = telefone.replace(/\D/g, ''); 
+            telefoneFormatado = telefoneFormatado.replace(/(\d{2})(\d{4,5})(\d{4})$/, '($1) $2-$3');
+
+            return telefoneFormatado;
+        }
+
+        function atualizarCampoTelefone() {
+            var campoTelefone = document.getElementById('telefone');
+            campoTelefone.value = formatarTelefone(campoTelefone.value);
+        }
+    </script>
 </head>
 <body>
-<section>
+    <section>
         <h2>Adicionar Novo Cliente</h2>
-        <p>Como vendedor, quero adicionar novos clientes à minha lista, para que eu possa ter controle de acesso aos meus clientes.</p>
 
-        <form action="processar_cliente.php" method="post">
+        <form action="processar-cliente.php" method="post">
             <label for="nome">Nome:</label>
             <input type="text" name="nome" required>
 
             <label for="divida">Dívida Atual:</label>
-            <input type="text" name="divida" required>
+            <input type="text" name="divida" id="divida" oninput="atualizarCampoDivida()" required>
 
             <label for="cpf">CPF:</label>
-            <input type="text" name="cpf">
+            <input type="text" name="cpf" id="cpf" oninput="atualizarCampoCPF()" maxlength="14">
 
             <label for="endereco">Endereço:</label>
             <input type="text" name="endereco">
 
             <label for="telefone">Telefone:</label>
-            <input type="text" name="telefone">
+            <input type="text" name="telefone" id="telefone" oninput="atualizarCampoTelefone()" maxlength="15">
 
             <input type="submit" value="Adicionar Cliente">
         </form>
