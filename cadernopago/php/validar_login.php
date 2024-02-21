3935b5298d3ba0e092a5f9a151d26bb9b36dd84f
fguilhermeICLS <146739245+fguilhermeICLS@users.noreply.github.com>
2024-02-21 14:38:53 -0300

    Update validar_login.php

--- cadernopago/php/validar_login.php
+++ cadernopago/php/validar_login.php
@@ -9,8 +9,7 @@ try {
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $email = $_POST["emailLogin"];
         $senha = $_POST["senhaLogin"];
-
-        // Consulta preparada para evitar injeção de SQL
+        
         $stmt = $conexao->prepare("SELECT id, senha FROM usuarios WHERE email = ?");
         $stmt->bind_param("s", $email);
         $stmt->execute();
@@ -18,19 +17,15 @@ try {
 
         if ($stmt->fetch()) {
             if (password_verify($senha, $hashSenha)) {
-                // Credenciais corretas, inicia a sessão
                 $_SESSION["usuario"] = $id;
                 header("Location: http://localhost/cadernopago/php/index.php");
             } else {
-                // Senha incorreta
                 $_SESSION["erro"] = "Senha incorreta. Por favor, tente novamente.";
                 header("Location: http://localhost/cadernopago/login.php");
             }
         } else {
-            // Usuário não encontrado
-            // Senha incorreta
             $_SESSION["erro"] = "Senha incorreta. Por favor, tente novamente.";
-            // Adiciona um parâmetro de erro na URL
+
             header("Location: http://localhost/cadernopago/login.php?erro=senha");
 
         }
@@ -40,7 +35,6 @@ try {
 
     $conexao->close();
 } catch (Exception $error) {
-    // Em caso de erro, armazena a mensagem de erro na sessão
     $_SESSION["erro"] = "Erro ao conectar com o banco de dados.";
     header("Location: http://localhost/cadernopago/login.php");
 }
