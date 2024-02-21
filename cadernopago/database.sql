0a7cf427446cd83393addc357585bd9db4baa53a
fguilhermeICLS <146739245+fguilhermeICLS@users.noreply.github.com>
2024-02-20 23:07:13 -0300

    Update database.sql

--- cadernopago/database.sql
+++ cadernopago/database.sql
@@ -1,3 +1,6 @@
+CREATE DATABASE cadernopago;
+
+use cadernopago;
 
 CREATE TABLE usuarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -8,3 +11,13 @@ CREATE TABLE usuarios (
     senha VARCHAR(255) NOT NULL,
     telefone VARCHAR(20) NOT NULL
 );
+
+CREATE TABLE clientes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    divida DECIMAL(10, 2) NOT NULL,
+    cpf VARCHAR(14),
+    endereco VARCHAR(255),
+    telefone VARCHAR(15)
+);
+
