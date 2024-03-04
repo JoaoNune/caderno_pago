50e8c0100ac4fef49e255efd4aca7d4cb2d35a4b
fguilhermeICLS <guilherme.leite61@aluno.ifce.edu.br>
2024-03-04 10:43:25 -0300

    correção

--- php/index.php
+++ php/index.php
@@ -11,105 +11,48 @@ if (isset($_SESSION["usuario"])) {
 
 <!DOCTYPE html>
 <html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Sistema Comercial</title>
-    <link rel="stylesheet" type="text/css" href="css/style.css"> 
-    <style>
-        body {
-    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-    margin: 0;
-    padding: 0;
-    background: linear-gradient(to right, #E0EAFC 0%, #CFDEF3 100%);
-    color: #333;
-    text-align: center;
-    height: 100vh;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-}
-
-header h1 {
-    margin: 0;
-    padding: 20px;
-    font-size: 2.5rem;
-    color: #205493;
-    background-color: #fff;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-}
-
-nav ul {
-    list-style-type: none;
-    padding: 0;
-    margin: 20px 0;
-    display: flex;
-    justify-content: center;
-    flex-wrap: wrap;
-}
-
-nav ul li {
-    margin: 0 10px;
-}
-
-nav ul li a {
-    text-decoration: none;
-    color: #205493;
-    background-color: #fff;
-    padding: 10px 15px;
-    border-radius: 5px;
-    transition: background-color 0.3s ease-in-out, color 0.3s ease-in-out;
-}
-
-nav ul li a:hover, nav ul li a:focus {
-    background-color: #E0EAFC;
-    color: #16325C;
-}
-
-footer {
-    position: fixed;
-    bottom: 0;
-    width: 100%;
-    padding: 10px;
-    color: #205493;
-    background-color: #fff;
-    box-shadow: 0 -2px 4px rgba(0, 0, 0, 0.1);
-}
-    </style>
-</head>
-<body>
-    <header>
+    <link rel="stylesheet" type="text/css" href="../css/style.css" />
+  </head>
+  <body>
+    <div class="container">
+      <header>
         <h1>Caderno Pago</h1>
-    </header>
-
-    <nav>
+      </header>
+      <nav class="menu-principal">
         <ul>
-            <li><a href="pesquisar-cliente.php">Pesquisar</a></li>
-            <li><a href="adicionar-cliente.php">Clientes</a></li>
-            <li><a href="#" id="configuracoesLink">Configurações</a></li>
-            <li><a href="#" id="filtrarLink">Filtrar</a></li>
-            <li><a href="?logoff" id="logoffLink">Logoff</a></li>
+          <li><a href="pesquisar-cliente.php">Pesquisar</a></li>
+          <li><a href="adicionar-cliente.php">Clientes</a></li>
+          <li>
+            <a href="#" id="configuracoesLink">Configurações</a>
+            <a href="#" id="filtrarLink">Filtrar</a>
+          </li>
+          <li><a href="?logoff" id="logoffLink">Logoff</a></li>
         </ul>
-    </nav>
-
-    <section>
-
-    </section>
+      </nav>
+    </div>
 
     <footer>
-        <p>&copy; 2024 Guilherme</p>
+      <p>&copy; 2024 Guilherme</p>
     </footer>
 
     <script>
-        document.getElementById("logoffLink").addEventListener("click", function(event) {
-            var confirmarLogoff = confirm("Tem certeza de que deseja fazer logoff?");
-            
-            if (!confirmarLogoff) {
-                event.preventDefault();
-            }
+      document
+        .getElementById("logoffLink")
+        .addEventListener("click", function (event) {
+          var confirmarLogoff = confirm(
+            "Tem certeza de que deseja fazer logoff?"
+          );
+
+          if (!confirmarLogoff) {
+            event.preventDefault();
+          }
         });
     </script>
-</body>
+  </body>
 </html>
 
 <?php
